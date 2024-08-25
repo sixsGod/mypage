e5cdba8575f0363fe1aea08ba3d9a9e6878021e8
allen <liusheng2008@qq.com>
2024-08-25 21:50:37 +0900

    update

--- index.js
+++ index.js
@@ -18,20 +18,18 @@ themeToggler.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme-variables');
 
     themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
-    classList.toggle('active');
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
-    classList.toggle('active');
 })
 
 //fill orders in table
-Orders.forEach(Order => {
+Orders.forEach(order => {
     const tr = document.createElement('tr');
-    const trConten = `
+    const trContent = `
                         <td>${order.productName}</td>
                         <td>${order.productNumber}</td>
                         <td>${order.paymentStatus}</td>
                         <td class="${order.shipping === 'Declined' ? 'danger' 
-                                   : order.shipping === 'pending' ? 'warning' 
+                                   : order.shipping === 'Pending' ? 'warning' 
                                    : 'primary'}"> ${order.shipping} </td>
                         <td class="primary">Details</td>
                         `;
